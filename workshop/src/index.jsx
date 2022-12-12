6454823e6697590c9c3660a371bea8b205d770b4
Yumi Kimura <kimurayumixy@gmail.com>
2022-12-12 11:48:56 +0900

    hello world done

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -2,18 +2,29 @@ import '../assets/stylesheets/application.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Hello = (props) => {
-  // const greeting = props.greeting;
-  // const name = props.name;
-  const { greeting, name } = props;
-  return (
-    <div>
-      <h1>{greeting} {name}!</h1>
-    </div>
-  );
-};
+// Functional React Component
+// const Hello = (props) => {
+//   // const greeting = props.greeting;
+//   // const name = props.name;
+//   const { greeting, name } = props;
+//   return (
+//     <div>
+//       <h1>{greeting} {name}!</h1>
+//     </div>
+//   );
+// };
 
 // Class react component
+class Hello extends React.Component {
+  render() {
+    const { greeting, name } = this.props;
+    return (
+      <div>
+        <h1>{greeting} {name}!</h1>
+      </div>
+    );
+  }
+}
 
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
