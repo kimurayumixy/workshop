ea468f6e5ace0c538e20e45ebe61a3f41ce43773
Yumi Kimura <kimurayumixy@gmail.com>
2022-12-12 11:10:29 +0900

    Hello world

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,20 @@
 import '../assets/stylesheets/application.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Hello = (props) => {
+  // const greeting = props.greeting;
+  // const name = props.name;
+  const { greeting, name } = props;
+  return (
+    <div>
+      <h1>{greeting} {name}!</h1>
+    </div>
+  );
+};
+
+// Class react component
+
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+root.render(<Hello name="Erika" greeting="Hej" />);
